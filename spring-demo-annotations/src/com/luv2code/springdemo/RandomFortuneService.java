b6be95936f33894205369ce11b98fc58817d0174
Mihail Merkov <mihailmerkov@gmail.com>
2019-09-18 11:49:53 +0300

    creation of RandomFortuneService class

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+	private String[] data = {
+			"Pink flower",
+			"Blue sea",
+			"Yellow sun"
+	};
+	
+	private Random myRandom = new Random();
+	
+	
+	
+	@Override
+	public String getFortune() {
+		int index = myRandom.nextInt(data.length);
+		
+		String theFortune = data[index];
+		
+		return theFortune;
+	}
+
+}
